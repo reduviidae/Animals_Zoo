d270a4237879a179ad2949884ec47a40f25ae49c
github username <github email address>
2018-12-13 12:01:23 -0500

    Second commit

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 class Animal
 
+  attr_reader :species, :nickname
+  attr_accessor :weight
+
+  @@animal_list = []
+
+  def initialize(species, weight, nickname)
+    @species = species
+    @weight = weight
+    @nickname = nickname
+    @@animal_list << self
+  end
+
+  def all # should return an array of all the animal instances
+    @@animal_list
+  end
+
 end
+
+lion = Animal.new("feline", 1000, "Simba")
+lion.all
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Zoo
   def initialize(name, location)
     @name = name
