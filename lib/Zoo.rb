cda27481c32e4c97b400d0c13edd778e6506f9ab
reduviidae <wiljago@gmail.com>
2018-12-13 11:48:53 -0500

    switching driver and navigator

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,21 @@
 class Zoo
+  def initialize(name, location)
+    @name = name
+    @location = location
+  end
+
+  def location
+    @location
+  end
+
+  def name
+    @name
+  end
+
+  @@name = []
+
+  def all
+    @@name
+  end
 
 end
